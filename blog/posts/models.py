a10b9914d48750209cdf2b095454336c354e97d2
David Brown <browndj24@googlemail.com>
2019-03-15 14:40:48 +0000

    created Posts model

--- blog/posts/models.py
+++ blog/posts/models.py
@@ -1,3 +1,9 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+
+
+class Posts(models.Model):
+    creator = models.ForeignKey(User, related_name='posts')
+    creation_date = models.DateTimeField(auto_now_add=True)
+    
