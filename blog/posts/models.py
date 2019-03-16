5774c5422910499ccd24781e7ff0411f5003c64e
David Brown <browndj24@googlemail.com>
2019-03-16 19:42:17 +0000

    git repo reset and recommit multiple changes

--- blog/posts/models.py
+++ blog/posts/models.py
@@ -11,9 +11,29 @@ class Posts(models.Model):
     )
     creation_date = models.DateTimeField(auto_now_add=True)
     url = models.URLField()
-    upvotes = models.ManyToManyField(User, through='PostUpvote')
+    title = models.CharField(max_length=256)
+    upvotes = models.ManyToManyField(User, through='PostUpvote', related_name='posts_upvotes')
 
 
 class PostUpvote(models.Model):
     post = models.ForeignKey(Posts, on_delete=models.CASCADE)
     user = models.ForeignKey(User, related_name='upvotes', on_delete=models.CASCADE)
+
+
+class Comment(models.Model):
+    creation_date = models.DateTimeField(auto_now_add=True)
+    creator = models.ForeignKey(
+        User,
+        related_name='comments',
+        on_delete=models.SET_NULL,
+        null=True,
+    )
+    post = models.ForeignKey(Posts, related_name='comments', on_delete=models.CASCADE)
+    parent = models.ForeignKey('Comment', related_name='replies', on_delete=models.CASCADE,
+                               null=True, default=None)
+    content = models.TextField(null=True)
+
+
+class CommentUpvote(models.Model):
+    comment = models.ForeignKey(Comment, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, related_name='comment_upvotes', on_delete=models.CASCADE)
