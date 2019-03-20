a86991f646bda6480cc086210b5cf295d5b88910
David Brown <browndj24@googlemail.com>
2019-03-20 19:29:40 +0000

    refactored and fixed Posts model

--- blog/posts/models.py
+++ blog/posts/models.py
@@ -3,7 +3,6 @@ from datetime import datetime, timedelta
 from django.db import models
 from django.contrib.auth.models import User
 from django.template.defaultfilters import pluralize
-# Create your models here.
 
 
 class Posts(models.Model):
@@ -26,9 +25,11 @@ class Posts(models.Model):
         if how_long < timedelta(minutes=1):
             return f'{how_long.seconds} second{pluralize(how_long.seconds)} ago'
         elif how_long < timedelta(hours=1):
-            return f'{how_long.minutes} minute{pluralize(how_long.minutes)} ago'
+            minutes = int(how_long.total_seconds()) // 60
+            return f'{minutes} minute{pluralize(minutes)} ago'
         elif how_long < timedelta(days=1):
-            return f'{how_long.hours} hour{pluralize(how_long.hours)} ago'
+            hours = int(how_long.total_seconds()) // 3600
+            return f'{hours} hour{pluralize(hours)} ago'
         else:
             return f'{how_long.days} day{pluralize(how_long.days)} ago'
 
