1531c1262b570557de0987cd5be3f6cead13c951
David Brown <browndj24@googlemail.com>
2019-03-20 15:34:46 +0000

    added multiple seconds test and first draft project README

--- blog/posts/tests.py
+++ blog/posts/tests.py
@@ -24,3 +24,12 @@ class PostsTestCase(TestCase):
             dt.now = mock.Mock()
             dt.now.return_value = creation + timedelta(seconds=1)
             self.assertEqual(post.how_long_ago(), '1 second ago')
+
+    def test_how_long_multiple_seconds(self):
+        creation = datetime(year=1966, month=6, day=6)
+
+        post = Posts(publication_date=creation)
+        with mock.patch('blog.posts.models.datetime') as dt:
+            dt.now =mock.Mock()
+            dt.now.return_value = creation +timedelta(seconds=42)
+            self.assertEqual(post.how_long_ago(), '42 seconds ago')
