9adb426e8a983b60954a9d9ddf7ecff7a71b8cb8
David Brown <browndj24@googlemail.com>
2019-03-20 13:00:48 +0000

    added test for publication 1 second ago how_long_1_sec_ago

--- blog/posts/tests.py
+++ blog/posts/tests.py
@@ -15,3 +15,12 @@ class PostsTestCase(TestCase):
             dt.now = mock.Mock()
             dt.now.return_value = creation
             self.assertEqual(post.how_long_ago(), '0 seconds ago')
+
+    def test_how_long_1_sec_ago(self):
+        creation = datetime(year=1966, month=6, day=6)
+
+        post = Posts(publication_date=creation)
+        with mock.patch('blog.posts.models.datetime') as dt:
+            dt.now = mock.Mock()
+            dt.now.return_value = creation + timedelta(seconds=1)
+            self.assertEqual(post.how_long_ago(), '1 second ago')
