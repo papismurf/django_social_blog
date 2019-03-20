ff9bc96bd49d04ca42a351e12be917bddb472353
David Brown <browndj24@googlemail.com>
2019-03-20 12:43:54 +0000

    created PostTestCase with test method how_long_0_secs for post publication date

--- blog/posts/tests.py
+++ blog/posts/tests.py
@@ -1,3 +1,17 @@
+from datetime import datetime, timedelta
+from unittest import mock
+
 from django.test import TestCase
 
-# Create your tests here.
+from .models import Posts
+
+
+class PostsTestCase(TestCase):
+    def test_how_long_0_secs(self):
+        creation = datetime(year=1966, month=6, day=6)
+
+        post = Posts(publication_date=creation)
+        with mock.patch('blog.posts.models.datetime') as dt:
+            dt.now = mock.Mock()
+            dt.now.return_value = creation
+            self.assertEqual(post.how_long_ago(), '0 seconds ago')
